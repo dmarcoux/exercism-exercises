a6dd998fed260286ad2dbd0ed4a791982c95ebe6
Dany Marcoux <git@dmarcoux.com>
2024-05-20 15:01:42 +0200

    Finish high scores

---
+++ csharp/high-scores/HighScores.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighScores(List<int> scores)
+{
+    public List<int> Scores() => scores;
+
+    public int Latest() => scores.Last();
+
+    public int PersonalBest() => scores.Max();
+
+    public List<int> PersonalTopThree() => scores.OrderDescending().Take(3).ToList();
+}
